24be7057c0f498c108f4169bdd8a1cfc1bb2c75a
ximeng <ximeng1105@163.com>
2026-07-06 22:52:34 +0800

    初步实现check类，有检查学号、班级号、宿舍号、床位号、学生姓名是否合法的5个功能，使用static声明，可以在不创建check类对象的情况下调用

---
+++ src/system/check.cpp
@@ -0,0 +1,40 @@
+#include "check.h"
+#include <QString>
+
+bool check::is_valid_student_id(int id)//检查学号是否合法(10000000~99999999)
+{
+	if(id < 10000000 || id > 99999999)
+		return false;
+
+	return true;
+}
+
+bool check::is_valid_class_num(int class_num)//检查班级号是否合法(1~99)
+{
+	if (class_num < 1 || class_num > 99)
+		return false;
+	return true;
+}
+
+bool check::is_valid_dorm_id(int dorm_id)//检查宿舍号是否合法(1001~9999)
+{
+	if (dorm_id < 1001 || dorm_id > 9999)
+		return false;
+	return true;
+}
+
+bool check::is_valid_bed_id(int bed_id, int max_num)//检查床位号是否合法(1~max_num)
+{
+	if (bed_id < 1 || bed_id > max_num)
+		return false;
+	return true;
+}
+
+bool check::is_valid_student_name(const QString& name)//检查学生姓名是否合法(1~20个字符,禁止error)
+{
+	if (name.length() < 1 || name.length() > 20)
+		return false;
+	if (name.contains("error"))
+		return false;
+	return true;
+}
---
+++ src/system/check.h
@@ -0,0 +1,17 @@
+#ifndef CHECK_H
+#define CHECK_H
+
+#include <QString>
+
+class check
+{
+public:
+	static bool is_valid_student_id(const QString& id);//检查学号是否合法(10000000~99999999)
+	static bool is_valid_class_num(int class_num);//检查班级号是否合法(1~99)
+	static bool is_valid_dorm_id(int dorm_id);//检查宿舍号是否合法(1001~9999)
+	static bool is_valid_bed_id(int bed_id, int max_num);//检查床位号是否合法(1~max_num)
+	static bool is_valid_student_name(const QString& name);//检查学生姓名是否合法(1~20个字符,禁止error)
+};
+
+
+#endif // CHECK_H
